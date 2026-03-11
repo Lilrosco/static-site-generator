8755db95c03ee578b8479cd9370bf98617234776
Lilrosco <rossey.charleston@gmail.com>
2026-03-11 18:44:42 -0400

    Now able to generate a web page from markdown.

--- main.sh
+++ main.sh
@@ -1,1 +1,10 @@
+PUBLIC_DIRECTORY="./public"
+
+# if [ -d "$PUBLIC_DIRECTORY" ]; then
+#    echo "$PUBLIC_DIRECTORY already exists"
+# else
+#     mkdir $PUBLIC_DIRECTORY
+# fi
+
 python3 src/main.py
+cd public && python3 -m http.server 8888
--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -4,7 +4,7 @@ from leafnode import LeafNode
 from parentnode import ParentNode
 
 
-class TestLeafNode(unittest.TestCase):
+class TestParentNode(unittest.TestCase):
     def test_to_html_with_children(self):
         child_node = LeafNode("span", "child")
         parent_node = ParentNode("div", [child_node])
